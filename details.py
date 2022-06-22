8a2bd77bca87f6de1da5529bd081c276ac30f040
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2022-06-22 17:34:20 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 18516364
-api_hash = "2e90970ea9e3ad55a3d7566754274fae"
-bot_token = "2057624136:AAGqDUgyUmtNZjeaO-ySc-YYJhTomhS1hmA"
+api_id = 18860540
+api_hash = "22dd2ad1706199438ab3474e85c9afab"
+bot_token = "5481058188:AAER9CEsDAQEnT5ffzq2o53G6pLjYh_HAvE"
 
