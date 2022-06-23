364cd215ed606f00c02794c20aa2b7dc25728c28
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2022-06-23 23:31:20 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -43,9 +43,9 @@ bot = Client(
 #    api_id=int(os.environ.get("API_ID")),
 #    api_hash=os.environ.get("API_HASH")
 #)
-#auth_users = [ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
-#sudo_users = auth_users
-#sudo_groups = [ int(chat) for chat in os.environ.get("GROUPS").split(",")  if chat != '']
+auth_users = [ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
+sudo_users = auth_users
+sudo_groups = [ int(chat) for chat in os.environ.get("GROUPS").split(",")  if chat != '']
 
 @bot.on_message(filters.command(["start"])&  (filters.chat(sudo_groups)))
 async def account_login(bot: Client, m: Message):
