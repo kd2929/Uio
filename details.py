f23054822030302367ee65e60d7622db831ca958
Devid176 <103266119+Devid176@users.noreply.github.com>
2022-07-30 22:41:13 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "15267676"
 api_hash = "fbd4362f9395fda1e947d459e2d012a4"
 bot_token = "5464011113:AAErw3mC0BXq6cY6sOuEjBbSJ_28BXxEWDI"
-sudo_groups = -1001760144773
+sudo_groups = -1001649615015
