4fe2f803b9822b0c0812539493340a1301e310db
Devid176 <103266119+Devid176@users.noreply.github.com>
2023-01-29 13:43:49 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -27,7 +27,7 @@ import sys
 import re
 import os
 # import pycurl
-
+# By... heArt🖤
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Language': 'en-US,en;q=0.9',
