d1752a3066b0c04d56ec4a2cfdb2415feaf2adb3
Mayank0004 <110011878+Mayank0004@users.noreply.github.com>
2023-02-03 11:24:54 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "29258160"
 api_hash = "6456101ce407f54d0a7fabb456b00b23"
-bot_token = "5699064315:AAG0tLUlkMMK0dF31xUmtDjQbB39MLseUTw"
-sudo_groups = -1001747003399
+bot_token = "6071914878:AAGNsujyIuafVqHgw5ILbD7s3-PL-qOPsYY"
+sudo_groups = -1001893504563
