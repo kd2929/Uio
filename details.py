79aa7b43924b7084ffe5942ff5ee23c095ae5984
Drishyam55 <123865384+Drishyam55@users.noreply.github.com>
2023-02-08 21:26:40 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "21609776"
 api_hash = "61f2558d634df353a2a9806661f4b091"
-bot_token = "5830575251:AAFsl1bZooAR24TWFatSWXymp5Pssq9zccM"
+bot_token = "6011730935:AAHYiqoTcYtcbX94QG2sfnw9wjtZt5otxn0"
 sudo_groups = -1001855421199
