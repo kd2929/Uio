fb766a2641998877b38e2eac02ae93af1718a572
DxJiddi6708 <124843630+DxJiddi6708@users.noreply.github.com>
2023-02-14 07:56:02 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "21609776"
 api_hash = "61f2558d634df353a2a9806661f4b091"
 bot_token = "5464011113:AAErw3mC0BXq6cY6sOuEjBbSJ_28BXxEWDI"
-sudo_groups = -1001855421199
+sudo_groups = -1001870874139
