2bad9329bcc0bafcf9148cba4b5b61338748f3c9
Adarshpandeyji <123234273+Adarshpandeyji@users.noreply.github.com>
2023-06-02 15:58:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -604,14 +604,10 @@ async def adda_pdf(bot: Client, m: Message):
     await m.reply_text("Done")
 
 
+
 @bot.on_message(filters.command(["jw"]))
-async def account_login(bot: Client, m: Message):
-    user = m.from_user.id if m.from_user is not None else None
-    if user is not None and user not in sudo_users:
-        await m.reply("**TUM BHOSADI WALE NIKKAL LO**", quote=True)
-        return
-    else:
-        editable = await m.reply_text(
+async def account_login(bot: Client, m: Message):   
+editable = await m.reply_text(
             "Hello Bruh **I am jw Downloader Bot**. I can download videos from **text** file one by one.**\n\nLanguage** : Python**\nFramework** :Pyrogram\n\nSend **TXT** File {Name : Link}"
        ,reply_markup=keyboard)
     input: Message = await bot.listen(editable.chat.id)
