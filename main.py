4868d9f21885c20d4c9e11c4eab7e42b8ddfd13d
Adarshpandeyji <123234273+Adarshpandeyji@users.noreply.github.com>
2023-06-07 14:33:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ async def account_login(bot: Client, m: Message):
     input2: Message = await bot.listen(editable.chat.id)
     raw_text2 = input2.text
     
-   await m.reply_text("**Downloaded By*")
+   await m.reply_text("**Downloaded By**")
     input3: Message = await bot.listen(editable.chat.id)
     raw_text3 = input3.text
 
