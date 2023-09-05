f10a4f8dd47c5c4d61699879bae1b41ddb35f0cf
Adarshpandeyji <123234273+Adarshpandeyji@users.noreply.github.com>
2023-09-05 20:28:21 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "29674733"
 api_hash = "f2bc694be25a0abe2417a9aa5e81dcb2"
-bot_token = "6464210758:AAH4sJWiXrIfox7Lp9fF67fvsgCROcQNrPE"
+bot_token = "6038626768:AAF0qnofudOZSDu8qxmD0WcnrhW9aFFQQj0"
 sudo_groups = -1001582364574
