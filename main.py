f8bbe65fdaa53f80506518b889d8d71bb77a2f01
Adarshpandeyji <123234273+Adarshpandeyji@users.noreply.github.com>
2023-09-09 21:04:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -776,9 +776,9 @@ async def account_login(bot: Client, m: Message):
 
             prog = await m.reply_text(Show)
 
-            cc = f'**Title »** {name1}.mp4\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Downloaded By** :- Black Adam'
+            cc = f'**Title »** {name1}.mp4\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Downloaded By** :- Bʟᴀᴄᴋ⚡Ꭺᴅᴀᴍ️'
 
-            cc1 = f'**Title »** {name1}.pdf\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Downloaded By** :- Black Adam'
+            cc1 = f'**Title »** {name1}.pdf\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Downloaded By** :- Bʟᴀᴄᴋ⚡Ꭺᴅᴀᴍ️'
 
             try:
 
