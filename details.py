4196ced21f4f91bcf78d72424973baae048610b7
Vivekfhfd <132126550+Vivekfhfd@users.noreply.github.com>
2023-11-07 22:37:03 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "29674733"
 api_hash = "f2bc694be25a0abe2417a9aa5e81dcb2"
 bot_token = "6146421293:AAGAlBcAtnvBRp-Jo_e6DLkJKLfGbzxWh7I"
-sudo_groups = -1001733798297
+sudo_groups = -1001852474262
