1ab9be8a528b1a305a9c6618047d2365bf0870e0
Lord <110597375+kd2929@users.noreply.github.com>
2023-11-09 13:00:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,12 +57,11 @@ visionias_quality_dict = {
 visionias_url_extract_pattern = r"(https://.*?playlist.m3u8.*?)\""
 
  bot = Client(
-    "bot",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH")
- )
- 
+     "bot",
+      api_id=api_id,
+      api_hash=api_hash, 
+      bot_token=bot_token)
+
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
 
